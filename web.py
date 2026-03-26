d0fa7a40ff21cd0bfa3e7352eb0e16b1cec157b1
a0989100151-pixel <a0989100151@gmail.com>
2026-03-26 21:31:04 +0800

    flask網站v2

--- web.py
+++ web.py
@@ -1,19 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    link = "<h1>歡迎進入黃福恩的網站</h1>"
+    link = "<h1>歡迎進入黃福恩的網站首頁</h1>"
     link += "<a href=/mis>課程</a><hr>"
     link += "<a href=/today>現在日期時間</a><hr>"
     link += "<a href=/me>關於我</a><hr>"
+    link += "<a href=/welcome?u=福恩&d=靜宜資管&c=資訊管理導論>GET傳值</a><hr>"
+    link += "<a href=/account>POST傳值</a><hr>"
+    link += "<a href=/math>次方與根號計算</a><hr>"
     return link
 
 @app.route("/mis")
 def course():
-    return "<h1>資訊管理導論</h1>"
+    return "<h1>資訊管理導論</h1><a href=/>返回首頁</a>"
 
 @app.route("/today")
 def today():
@@ -23,6 +26,54 @@ def today():
 @app.route("/me")
 def me():
     return render_template("about.html")
+
+@app.route("/welcome", methods=["GET"])
+def welcome():
+    user = request.values.get("u")
+    d = request.values.get("d")
+    c = request.values.get("c")    
+    return render_template("welcome.html", name= user, dep = d, course = c)
+
+
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    if request.method == "POST":
+        user = request.form["user"]
+        pwd = request.form["pwd"]
+        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
+        return result
+    else:
+        return render_template("account.html")
+
+@app.route("/math", methods=["GET", "POST"])
+def math():
+    result = None
+    x = y = opt = None
+
+    if request.method == "POST":
+        try:
+            x = float(request.form["x"])
+            opt = request.form["opt"]
+            y = float(request.form["y"])
+
+            if opt == "∧":
+                result = x ** y
+
+            elif opt == "√":
+                if x < 0:
+                    result = "負數不能開根號"
+                elif y == 0:
+                    result = "根號次數不能為0"
+                else:
+                    result = x ** (1 / y)
+
+            else:
+                result = "未知運算"
+
+        except:
+            result = "輸入錯誤"
+
+    return render_template("math.html", result=result, x=x, y=y, opt=opt)
 
 if __name__ == "__main__":
     app.run(debug=True)
